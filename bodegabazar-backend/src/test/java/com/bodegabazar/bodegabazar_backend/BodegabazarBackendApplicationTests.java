388738c73f6bff1d5239c6014fc57fa5f8f09e9a
onlyjack03 <sting.gonzales03@gmail.com>
2025-06-09 19:49:18 -0500

    Add files via upload

---
+++ bodegabazar-backend/src/test/java/com/bodegabazar/bodegabazar_backend/BodegabazarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodegabazar.bodegabazar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegabazarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
